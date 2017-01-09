143c1f1544380d82bf715684f405da053fd46f52
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2017-01-09 23:32:37 +0200

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by Karim Ebrahem on 08/01/2017.
+ */
+public class Assignment {
+    public int grade;
+    public String owner;
+    public String solution;
+
+    public Assignment(){}
+    public Assignment(int grade, String owner, String solution){
+        this.grade = grade;
+        this.owner = owner;
+        this.solution = solution;
+    }
+}
